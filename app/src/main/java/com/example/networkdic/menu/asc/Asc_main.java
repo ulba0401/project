9ccc85aefb3ddcb46044290443beca6f1ae3518f
ulba0401 <ulba0401@naver.com>
2019-05-25 14:14:38 +0900

    인덱스스크롤 만드는중..(수정중)

--- app/src/main/java/com/example/networkdic/menu/asc/Asc_main.java
+++ app/src/main/java/com/example/networkdic/menu/asc/Asc_main.java
@@ -8,13 +8,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SectionIndexer;
 
 import com.example.networkdic.MainActivity;
+import com.example.networkdic.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.networkdic.R.layout.asc_main_fragment;
 
 public class Asc_main extends Fragment {
     MainActivity activity;
+    private ArrayList<String> mItems;
+    private IndexableListView mListView;
 
     @Override
     public void onAttach(Context context) {
@@ -32,6 +40,54 @@ public class Asc_main extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(asc_main_fragment, container, false);
+        ContentAdapter adapter = new ContentAdapter(getContext(),android.R.layout.simple_list_item_1, mItems);
+
+        mListView = rootView.findViewById(R.id.asc_list);
+        mListView.setAdapter(adapter);
+        mListView.setFastScrollEnabled(true);
         return rootView;
+    }
+
+    private class ContentAdapter extends ArrayAdapter<String> implements SectionIndexer {
+
+        private String mSections = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public ContentAdapter(Context context, int textViewResourceId,
+                              List<String> objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public int getPositionForSection(int section) {
+            // If there is no item for current section, previous section will be selected
+            for (int i = section; i >= 0; i--) {
+                for (int j = 0; j < getCount(); j++) {
+                    if (i == 0) {
+                        // For numeric section
+                        for (int k = 0; k <= 9; k++) {
+                            if (StringMatcher.match(String.valueOf(getItem(j).charAt(0)), String.valueOf(k)))
+                                return j;
+                        }
+                    } else {
+                        if (StringMatcher.match(String.valueOf(getItem(j).charAt(0)), String.valueOf(mSections.charAt(i))))
+                            return j;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        @Override
+        public int getSectionForPosition(int position) {
+            return 0;
+        }
+
+        @Override
+        public Object[] getSections() {
+            String[] sections = new String[mSections.length()];
+            for (int i = 0; i < mSections.length(); i++)
+                sections[i] = String.valueOf(mSections.charAt(i));
+            return sections;
+        }
     }
 }
